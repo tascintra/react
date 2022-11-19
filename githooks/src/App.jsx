0053d57bad6f44a3e44351235e9b61f8755e4dad
tascintra <tiago.slv@hotmail.com>
2022-11-18 23:54:22 -0300

    created repo favorite

--- githooks/src/App.jsx
+++ githooks/src/App.jsx
@@ -1,26 +1,35 @@
-import React, { useState } from "react"
+import { useState, useEffect } from "react"
 
 export default function App() {
-  const [repositories, setRepositories] = useState([
-    { id: 1, name: "repo-1" },
-    { id: 2, name: "repo-2" },
-    { id: 3, name: "repo-3" },
-  ])
- 
-  function handleAddRepository() {
-    setRepositories([...repositories, { id: Math.random(), name: "Novo repo" }])
+  const [repositories, setRepositories] = useState([])
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch("https://api.github.com/users/tascintra/repos")
+      const data = await response.json()
+      setRepositories(data)
+    }
+    fetchData();
+  }, [])
+
+  function handleFavorite(id) {
+    const newRepositories = repositories.map(repo => {
+      return repo.id === id ? { ...repositories, favorite:true } : repo
+    })
+
+    setRepositories(newRepositories)
   }
 
   return (
-    <>
-      <ul>
-        { repositories.map( repo => (
-          <li key={repo.id}>{repo.name}</li>
-        ))}
-      </ul>
-      <button onClick={handleAddRepository}>
-        Adicionar repositório
-      </button>
-    </>
+    <ul>
+      { repositories.map( repo => (
+        <li key={repo.id}>
+          {repo.name}
+          {repo.favorite && <span>(Favorito)</span>}
+          <button onClick={() => handleFavorite(repo.id)}>Favoritar</button>
+        </li>
+      ))}
+    </ul>
+
   )
 }
