e8746c9359cfdc4cec01d133c164dff82bb0eb98
tascintra <tiago.slv@hotmail.com>
2022-11-19 01:54:03 -0300

    created function to get browser position

--- navlocation/src/App.jsx
+++ navlocation/src/App.jsx
@@ -1,11 +1,20 @@
 import { useState, useEffect } from 'react'
 
 function App() {
-  
+  const [location, setLocation] = useState({})
+
+  useEffect(() => {
+    navigator.geolocation.watchPosition(handlePositionReceived)
+  }, [])
+
+  function handlePositionReceived(coordinates) {
+    console.log(coordinates)
+  }
 
   return (
     <>
-      
+      Latitude: 34.33 <br />
+      Longitude: 1243432556767
     </>
   )
 }
