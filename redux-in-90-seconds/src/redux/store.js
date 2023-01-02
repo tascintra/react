4d75b0a5ca408a44258456a61a466bc4f0434742
Tiago Cintra <tiago.slv@hotmail.com>
2023-01-02 20:55:19 -0300

    configured the store reducer

--- redux-in-90-seconds/src/redux/store.js
+++ redux-in-90-seconds/src/redux/store.js
@@ -1,3 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { useReducer } from './userSlice';
 
-export default configureStore({});
+export default configureStore({
+  reducer: {
+    user: useReducer,
+  }
+});
