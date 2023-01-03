c425a9cfc6243eca01f0ac01d67fe9d61e28ff4f
Tiago Cintra <tiago.slv@hotmail.com>
2023-01-02 21:12:04 -0300

    userShow component done

---
+++ redux-in-90-seconds/src/components/UserShow.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+
+const UserShow = () => {
+  const { name } = useSelector((state) => state.user);
+
+  return (
+    <label>Teste</label>
+  )
+};
