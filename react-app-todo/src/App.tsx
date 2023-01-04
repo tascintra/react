d8ec1a62eb046b83831c6e537ba86ceb50f99eda
Tiago Cintra <tiago.slv@hotmail.com>
2023-01-03 23:04:15 -0300

    added usestate and useeffect to app

--- react-app-todo/src/App.tsx
+++ react-app-todo/src/App.tsx
@@ -1,11 +1,20 @@
-import './App.css'
+import { useState, useEffect } from 'react';
+import './App.css';
 
 function App() {
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+
+  }, []);
+
+  // https://jsonplaceholder.typicode.com/todos
+
   return (
     <div className="App">
       Hello World
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
