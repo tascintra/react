1479816047e155d8b4e3b9d979a15a878840051b
Tiago Cintra <tiago.slv@hotmail.com>
2023-01-08 22:30:14 -0300

    created todo component structure and made the import and used inside react app

--- react-app-todo/src/App.tsx
+++ react-app-todo/src/App.tsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import './App.css';
 import Loader from './components/Loader';
+import Todo from './components/Todo';
 
 function App() {
   const [todos, setTodos] = useState([]);
@@ -23,9 +24,7 @@ function App() {
 
   return (
     <div className='App'>
-      {todos.length > 0
-        ? todos.map((todo: Todo) => console.log(todo.title))
-        : (<Loader/>)}
+      {todos.length > 0 ? todos.map((todo: Todo) => <Todo />) : <Loader />}
     </div>
   );
 }
---
+++ react-app-todo/src/components/Todo.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Todo = () => {
+  return (<>Todo ...</>)
+}
+
+export default Todo;
